59bf54d64242889c5e797702776b84c32ec74f15
Kuba Kwiatkowski <jakub.kwiatkowski1981@gmail.com>
2020-02-16 17:55:10 +0100

    BEM wstep + js poczatkowa ingerencja w kod html

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ const name = 'Kuba';
 const age = 38;
 console.log(`Witam Ogarze na mojej stronie, nazywam się ${name} i mam ${age} lat, wspaniale, ze tutaj jestes 🤪`);
 
+const diary = document.querySelector ('.diary__paragraph--js');
+console.log(diary.innerHTML);
+diary.innerHTML = '<b>Pozdro</b> JS!!!'
+
+const paragraphs = document.querySelectorAll ('p');
+console.log(paragraphs);
+console.log(paragraphs[2])
